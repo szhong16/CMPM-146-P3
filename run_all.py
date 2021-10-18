370714b1284ba9532a589850b6319e2ff2a620a0
Sam Feng <fengyuan20000303@gmail.com>
2021-10-18 15:08:21 -0700

    Create run_all.py

---
+++ run_all.py
@@ -0,0 +1,97 @@
+import subprocess
+import os, sys
+import random
+
+
+def show_match(bot, opponent_bot, map_num):
+    """
+        Runs an instance of Planet Wars between the two given bots on the specified map. After completion, the
+        game is replayed via a visual interface.
+    """
+    command = 'java -jar tools/PlayGame.jar maps/map' + str(map_num) + '.txt 1000 1000 log.txt ' + \
+              '"python ' + bot + '" ' + \
+              '"python ' + opponent_bot + '" ' + \
+              '| java -jar tools/ShowGame.jar'
+    print(command)
+    os.system(command)
+
+
+def test(bot, opponent_bot, map_num):
+    """ Runs an instance of Planet Wars between the two given bots on the specified map. """
+    bot_name, opponent_name = bot.split('/')[1].split('.')[0], opponent_bot.split('/')[1].split('.')[0]
+    print('Running test:',bot_name,'vs',opponent_name)
+    command = 'java -jar tools/PlayGame.jar maps/map' + str(map_num) +'.txt 1000 1000 log.txt ' + \
+              '"python ' + bot + '" ' + \
+              '"python ' + opponent_bot + '" '
+
+    print(command)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+    while True:
+        return_code = p.poll()  # returns None while subprocess is running
+        line = p.stdout.readline().decode('utf-8')
+        if '1 timed out' in line:
+            print(bot_name,'timed out.')
+            return None
+            break
+        elif '2 timed out' in line:
+            print(opponent_name,'timed out.')
+            return None
+            break
+        elif '1 crashed' in line:
+            print(bot_name, 'crashed.')
+            return None
+            break
+        elif '2 crashed' in line:
+            print(opponent_name, 'crashed')
+            return None
+            break
+        elif 'Player 1 Wins!' in line:
+            print(bot_name,'wins!')
+            return bot_name
+            break
+        elif 'Player 2 Wins!' in line:
+            print(opponent_name,'wins!')
+            return opponent_name
+            break
+
+        if return_code is not None:
+            break
+
+
+if __name__ == '__main__':
+    path =  os.getcwd()
+    opponents = ['opponent_bots/easy_bot.py',
+                 'opponent_bots/spread_bot.py',
+                 'opponent_bots/aggressive_bot.py',
+                 'opponent_bots/defensive_bot.py',
+                 'opponent_bots/production_bot.py']
+
+    a = random.randint(1,100)
+    # b = random.randint(1,100)
+    # c = random.randint(1,100)
+    # d = random.randint(1,100)
+    # e = random.randint(1,100)
+    maps = [a, a, a, a, a]
+    # maps = [71, 13, 24, 56, 7]
+
+    win_rate = {}
+
+    my_bot = 'behavior_tree_bot/bt_bot.py'
+    show = len(sys.argv) < 2 or sys.argv[1] == "show"
+    
+    # Run all maps
+    for i in range(10):
+        maps = [i, i, i, i, i]
+
+        for opponent, map in zip(opponents, maps):
+            result = test(my_bot, opponent, map)
+            
+            if result in win_rate.keys():
+                win_rate[result] += 1
+            else:
+                win_rate[result] = 1
+
+    print("- BATTLE RESULT -")
+    print(win_rate)
+
